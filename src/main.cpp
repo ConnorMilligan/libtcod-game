f52cafba3f5ec0c343009d6211e8e6a0e5a391ae
Connor Milligan <connoramilligan@gmail.com>
2023-02-16 12:14:00 -0500

    Changes tileset

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,38 @@
-#include <libtcod.h>
+#include <SDL.h>
+#include <libtcod.hpp>
 
-int main(void) {
+int main(int argc, char **argv) {
+  TCODConsole console(80, 25);
+  TCOD_ContextParams params{};
 
-  return 0;
+    params.tcod_version = TCOD_COMPILEDVERSION;  // This is required.
+  params.console = console.get();  // Derive the window size from the console size.
+  params.window_title = "Libtcod Project";
+  params.sdl_window_flags = SDL_WINDOW_RESIZABLE;
+  params.vsync = true;
+  params.argc = argc;  // This allows some user-control of the context.
+  params.argv = argv;
+
+  // Tileset example using a Code Page 437 font.
+  // "terminal8x8_gs_ro.png" must be in the working directory.
+  auto tileset = tcod::load_tilesheet("../../res/curses_640x300.png", {16, 16}, tcod::CHARMAP_CP437);
+  params.tileset = tileset.get();
+
+  auto context = tcod::Context(params);
+
+  while (1) {  // Game loop.
+    TCOD_console_clear(console.get());
+    tcod::print(console, {0, 0}, "Hello World", std::nullopt, std::nullopt);
+    context.present(console);  // Updates the visible display.
+
+    SDL_Event event;
+    SDL_WaitEvent(nullptr);  // Optional, sleep until events are available.
+    while (SDL_PollEvent(&event)) {
+      context.convert_event_coordinates(event);  // Optional, converts pixel coordinates into tile coordinates.
+      switch (event.type) {
+        case SDL_QUIT:
+          return 0;  // Exit.
+      }
+    }
+  }
 }
